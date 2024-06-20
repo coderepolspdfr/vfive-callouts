e0fb18394d107c2287bc4c51564ee00eff1c07f9
coderepolspdfr <imdatneek@icloud.com>
2024-06-20 13:56:04 +1000

    init

---
+++ VFive Callouts/Callouts/HighSpeedChase.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using LSPD_First_Response;
+using System.Drawing;
+using LSPD_First_Response.Mod.Callouts;
+using LSPD_First_Response.Mod.API;
+using System.Runtime.CompilerServices;
+using LSPD_First_Response.Engine;
+
+namespace VFive_Callouts.Callouts
+{
+    [CalloutInfo("HighSpeedChase", CalloutProbability.Medium)]
+
+    public class HighSpeedChase : Callout
+    {
+        private Ped Suspect;
+        private Vehicle SuspectVehicle;
+        private Blip SuspectBlip;
+        private LHandle Pursuit;
+        private Vector3 spawnpoint;
+        private bool PursuitCreated;
+
+        public override bool OnBeforeCalloutDisplayed()
+        {
+            spawnpoint = World.GetRandomPositionOnStreet();
+            ShowCalloutAreaBlipBeforeAccepting(spawnpoint, 30f);
+            AddMinimumDistanceCheck(30f, spawnpoint);
+            CalloutMessage = "High Speed Pursuit";
+            CalloutPosition = spawnpoint;
+            Functions.PlayScannerAudioUsingPosition("WE_HAVE_CRIME_RESISTING_ARREST_02 IN_OR_ON_POSITION", spawnpoint);
+            return base.OnBeforeCalloutDisplayed();
+        }
+
+        public override bool OnCalloutAccepted()
+        {
+            SuspectVehicle = new Vehicle("ADDER", spawnpoint);
+            SuspectVehicle.IsPersistent = true;
+
+            Suspect = new Ped(SuspectVehicle.GetOffsetPositionFront(5f));
+            Suspect.IsPersistent = true;
+            Suspect.BlockPermanentEvents = true;
+            Suspect.WarpIntoVehicle(SuspectVehicle, -1);
+
+            SuspectBlip = Suspect.AttachBlip();
+            SuspectBlip.Color = System.Drawing.Color.Red;
+            SuspectBlip.IsRouteEnabled = true;
+
+            SuspectVehicle.IsEngineOn = true;
+            SuspectVehicle.IsStolen = true;
+            SuspectVehicle.LicensePlate = "VFive";
+
+            PursuitCreated = false;
+            return base.OnCalloutAccepted();
+        }
+
+        public override void Process()
+        {
+            if (!PursuitCreated && Game.LocalPlayer.Character.DistanceTo(SuspectVehicle) <= 120f)
+            {
+                Pursuit = Functions.CreatePursuit();
+                Functions.AddPedToPursuit(Pursuit, Suspect);
+                Functions.SetPursuitIsActiveForPlayer(Pursuit, true);
+                PursuitCreated = true;
+            }
+
+            if (PursuitCreated && !Functions.IsPursuitStillRunning(Pursuit))
+            {
+                End();
+            }
+
+            base.Process();
+        }
+
+        public override void End()
+        {
+            base.End();
+
+            if (Suspect.Exists())
+            {
+                Suspect.Dismiss();
+            }
+
+            if (SuspectBlip.Exists())
+            {
+                SuspectBlip.Delete();
+            }
+
+            if (SuspectVehicle.Exists())
+            {
+                SuspectVehicle.Dismiss();
+            }
+
+            Game.LogTrivial("VFive Callouts: High Speed Pursuit ended.");
+        }
+    }
+}
---
+++ VFive Callouts/Main.cs
@@ -0,0 +1,69 @@
+﻿using LSPD_First_Response;
+using LSPD_First_Response.Mod.API;
+using Rage;
+using System.Reflection;
+
+namespace VFive_Callouts
+{
+    public class Main : Plugin
+    {
+        public string pluginversion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public override void Initialize()
+        {
+            Functions.OnOnDutyStateChanged += OnOnDutyStateChangedHandler;
+            Game.LogTrivial("\n-------------------------- VFive Callouts --------------------------\nVersion: " + pluginversion);
+
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(LSPDFRResolveEventHandler);
+        }
+
+        public override void Finally()
+        {
+            Game.LogTrivial("VFive Callouts cleaned up its own shit.");
+        }
+
+        private static void OnOnDutyStateChangedHandler(bool duty)
+        {
+            if (duty)
+            {
+                RegisterCallouts();
+                Game.DisplayNotification("VFive Callouts: \nVersion:" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + "\nStatus: Loaded!");
+            }
+        }
+
+        private static void RegisterCallouts()
+        {
+            Functions.RegisterCallout(typeof(Callouts.HighSpeedChase));
+        }
+
+        public static Assembly LSPDFRResolveEventHandler(object sender, ResolveEventArgs args)
+        {
+            foreach (Assembly assembly in Functions.GetAllUserPlugins())
+            {
+                if (args.Name.ToLower().Contains(assembly.GetName().Name.ToLower()))
+                {
+                    return assembly;
+                }
+            }
+
+            return null;
+        }
+
+        public static bool isLSPDFRPluginRunning(string Plugin, Version minver = null)
+        {
+            foreach (Assembly assembly in Functions.GetAllUserPlugins())
+            {
+                AssemblyName an = assembly.GetName();
+
+                if (an.Name.ToLower() == Plugin.ToLower())
+                {
+                    if (minver == null || an.Version.CompareTo(minver) >= 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
